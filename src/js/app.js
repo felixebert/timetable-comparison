38d8cb9fbeb822e66926f3293d91db9743b645cc
Felix Ebert <felix.ebert@if-core.de>
2013-12-19 12:32:37 +0100

    first design draft

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+var tc = {};
+
+(function(tc, angular) {
+	var ComparisonCtrl = function($scope) {
+		$scope.examples = tc.examples;
+	};
+
+	tc.ComparisonCtrl = ComparisonCtrl;
+})(tc, angular);
---
+++ src/js/data.js
@@ -0,0 +1,7 @@
+(function(tc) {
+	tc.examples = [{
+		name: 'Von Leingarten zur ASS'
+	}, {
+		name: 'Von Schwaigern zur ASS'
+	}];
+})(tc);
